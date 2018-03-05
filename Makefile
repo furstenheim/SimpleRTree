9de1f89147ef895526ab8d7f6bf6d2a6b3ce2381
gabriel <furstenheim@gmail.com>
2018-03-05 11:46:04 +0100

    Add load testing

--- Makefile
+++ Makefile
@@ -1,15 +1,28 @@
 debug:
 	dlv test -- -test.run Big$
 bench:
-	go test -v -bench=.
+	go test -v -bench=Find
 ## Show allocs per test
 bench-mem:
-	go test -v -bench=. -benchmem
+	go test -v -bench=Find -benchmem
 ## Show allocs trace
 bench-mem-trace:
 	go test -c
 	GODEBUG=allocfreetrace=1 ./SimpleRTree.test -test.run=none -test.benchtime=10ms -test.bench=BenchmarkSimpleRTree_FindNearestPointMemory 2>trace.log
 bench-graph:
 	mkdir -p benchmarks/$$(git rev-parse HEAD)
-	go test -run=XXX -bench . -cpuprofile benchmarks/$$(git rev-parse HEAD)/cpu.prof
+	go test -run=XXX -bench Find -cpuprofile benchmarks/$$(git rev-parse HEAD)/cpu.prof
+	go tool pprof -svg SimpleRTree.test benchmarks/$$(git rev-parse HEAD)/cpu.prof > benchmarks/$$(git rev-parse HEAD)/cpu.svgbench:
+bench-load:
+	go test -v -bench=Load
+## Show allocs per test
+bench-mem-load:
+	go test -v -bench=Load -benchmem
+## Show allocs trace
+bench-mem-trace-load:
+	go test -c
+	GODEBUG=allocfreetrace=1 ./SimpleRTree.test -test.run=none -test.benchtime=10ms -test.bench=BenchmarkSimpleRTree_LoadMemory 2>trace.log
+bench-graph-load:
+	mkdir -p benchmarks/$$(git rev-parse HEAD)
+	go test -run=XXX -bench Load -cpuprofile benchmarks/$$(git rev-parse HEAD)/cpu.prof
 	go tool pprof -svg SimpleRTree.test benchmarks/$$(git rev-parse HEAD)/cpu.prof > benchmarks/$$(git rev-parse HEAD)/cpu.svg
--- RTree_test.go
+++ RTree_test.go
@@ -49,7 +49,33 @@ func TestSimpleRTree_FindNearestPointBig(t *testing.T) {
 }
 
 
+func BenchmarkSimpleRTree_Load(b *testing.B) {
+	benchmarks := []struct{
+		name string
+		size int
+	}{
+		{"10", 10,},
+		{"1000", 1000,},
+		{"10000", 10000,},
+		{"100000", 100000,},
+		{"200000", 200000,},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func (b * testing.B) {
+			size := bm.size
+			points := make([]float64, size * 2)
+			for i := 0; i < 2 * size; i++ {
+				points[i] = rand.Float64()
+			}
+			fp := flatPoints(points)
 
+			b.ResetTimer()
+			for n := 0; n < b.N; n++ {
+				_ = New().Load(fp)
+			}
+		})
+	}
+}
 
 func BenchmarkSimpleRTree_FindNearestPoint(b *testing.B) {
 	benchmarks := []struct{
@@ -100,6 +126,28 @@ func BenchmarkSimpleRTree_FindNearestPointMemory(b *testing.B) {
 			for n := 0; n < b.N; n++ {
 				x, y := rand.Float64(), rand.Float64()
 				_, _, _ = r.FindNearestPoint(x, y)
+			}
+		})
+	}
+}
+func BenchmarkSimpleRTree_LoadMemory(b *testing.B) {
+	benchmarks := []struct{
+		name string
+		size int
+	}{
+		{"1000", 1000,},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func (b * testing.B) {
+			size := bm.size
+			points := make([]float64, size * 2)
+			for i := 0; i < 2 * size; i++ {
+				points[i] = rand.Float64()
+			}
+			fp := flatPoints(points)
+			b.ResetTimer()
+			for n := 0; n < b.N; n++ {
+				_ = New().Load(fp)
 			}
 		})
 	}
