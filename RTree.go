5bbaa6918351679e13c21394bebf441b584da9a9
gabriel <furstenheim@gmail.com>
2018-03-05 10:56:21 +0100

    Avoid math max

--- RTree.go
+++ RTree.go
@@ -302,31 +302,27 @@ func (n * Node) computeDistances (x, y float64) (mind, maxd float64) {
 	       d := (x - n.BBox.MinX) * (x - n.BBox.MinX)  + (y - n.BBox.MinY) * (y - n.BBox.MinY)
 	       return d, d
 	}
-	sides := [4]float64{
-		math.Min((x - n.BBox.MinX) * (x - n.BBox.MinX), (x - n.BBox.MaxX) * (x - n.BBox.MaxX)),
-		math.Max((x - n.BBox.MinX) * (x - n.BBox.MinX), (x - n.BBox.MaxX) * (x - n.BBox.MaxX)),
-		math.Min((y - n.BBox.MinY) * (y - n.BBox.MinY), (y - n.BBox.MaxY) * (y - n.BBox.MaxY)),
-		math.Max((y - n.BBox.MinY) * (y - n.BBox.MinY), (y - n.BBox.MaxY) * (y - n.BBox.MaxY)),
-	}
+	minx, maxx := sortFloats((x - n.BBox.MinX) * (x - n.BBox.MinX), (x - n.BBox.MaxX) * (x - n.BBox.MaxX))
+	miny, maxy := sortFloats((y - n.BBox.MinY) * (y - n.BBox.MinY), (y - n.BBox.MaxY) * (y - n.BBox.MaxY))
 
 	sideX := (n.BBox.MaxX - n.BBox.MinX) * (n.BBox.MaxX - n.BBox.MinX)
 	sideY := (n.BBox.MaxY - n.BBox.MinY) * (n.BBox.MaxY - n.BBox.MinY)
 
 	// fmt.Println(sides)
 	// point is inside because max distances in both axis are smaller than sides of the square
-	if (sides[1] < sideX && sides[3] < sideY) {
+	if (maxx < sideX && maxy < sideY) {
 		// do nothing mind is already 0
-	} else if (sides[1] < sideX) {
+	} else if (maxx < sideX) {
 		// point is in vertical stripe. Hence distance to the bbox is maximum vertical distance
-		mind = sides[2]
-	} else if (sides[3] < sideY) {
+		mind = miny
+	} else if (maxy < sideY) {
 		// point is in horizontal stripe, Hence distance is least distance to one of the sides (vertical distance is 0
-		mind = sides[0]
+		mind = minx
 	} else {
 		// point is not inside bbox. closest vertex is that one with closest x and y
-		mind = sides[0] + sides[2]
+		mind = minx + miny
 	}
-	maxd = sides[1] + sides[3]
+	maxd = maxx + maxy
 	return
 }
 
@@ -369,4 +365,11 @@ func minmaxFloatArray (s [4]float64) (min, max float64) {
 	       }
        }
        return min, max
+}
+
+func sortFloats (x1, x2 float64) (x3, x4 float64) {
+	if (x1 > x2) {
+		return x2, x1
+	}
+	return x1, x2
 }
